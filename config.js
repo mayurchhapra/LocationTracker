6c9dac675dfb656788e51edd40ea967432ab55a4
Mayur Chhapra <mayurchhapra@gmail.com>
2018-08-07 18:41:55 +0530

    Working Code

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    firebase: 'live-track-1533559724269',
+    mapbox: {
+        accessToken: 'pk.eyJ1IjoibWF5dXJjaGhhcHJhIiwiYSI6ImNqa2pobHplbDFmdGUzcm85Z2ZzYWpwNzAifQ.tqIKV_STkwiZa--ZUh3Qgw',
+        mapId: 'mattketmo.locpi8o0',
+    },
+    analytics: 'UA-23657373-6',
+  }
+  
